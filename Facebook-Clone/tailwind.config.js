7ac7cf998103928ef10ed3618cb62dfb1e48b05a
k29laksh <klaxay0505@gmail.com>
2024-03-11 14:58:48 +0530

    tailwind Projects

---
+++ Facebook-Clone/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      borderWidth: {
+        
+        1: '1px',
+        
+      },
+    },
+  },
+  plugins: [],
+}
+
